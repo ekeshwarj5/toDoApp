6ebdb6210f183a453226c199cf11743bb0aacb7b
Ekeshwar Jhode <mayurjhode55@gmail.com>
2024-01-02 22:27:32 +0530

    backend almost completed

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
-// mongodb+srv://mayurjhode55:<password>@cluster0.gaqmpjr.mongodb.net/
-mongoose.connect("mongodb+srv://mayurjhode55:<password>@cluster0.gaqmpjr.mongodb.net/");
-const toDoSchema = mongoose.Schema({
-    title : String,
-    decription : String,
-    completed : Boolean,
-
+mongoose.connect("mongodb+srv://mayurjhode55:EJjhode128@cluster0.gaqmpjr.mongodb.net/")
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
 })
 
-const toDo = mongoose.model('toDos', toDoSchema);
+const todo = mongoose.model('todos', todoSchema);
 
 module.exports = {
-    toDo
+    todo
 }
